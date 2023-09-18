77ca3b2cb49603f8e3f95dfe66e2eeba745f660f
stortrut <feiffalvin@gmail.com>
2023-09-18 11:32:14 +0200

    Enemy knockback och movement

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     public float movementSpeed = 10;
-
+    public float knockBackPower;
     Transform target;
-    Rigidbody2D rb2D;
+    Rigidbody2D rigidbody2D;
 
     private void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").transform;
-        rb2D = GetComponent<Rigidbody2D>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,16 +20,28 @@ public class EnemyScript : MonoBehaviour
     {
         Vector2 direction = target.position - transform.position;
         direction.Normalize();
-        rb2D.velocity = direction * movementSpeed;
+        rigidbody2D.velocity = direction * movementSpeed;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collision with thing");
 
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Collided with player");
+
+            Vector3 moveDirection = rigidbody2D.transform.position - collision.transform.position;
+            rigidbody2D.AddForce(-moveDirection.normalized * -knockBackPower);
+
+            Rigidbody2D playerRigidbody2D = collision.gameObject.GetComponent<Rigidbody2D>();
+            playerRigidbody2D.AddForce(moveDirection.normalized * -knockBackPower);
+
+        }
+
         //if (collision.gameObject.GetComponent<Laser>() != null)
         //{
-            
+
         //}
     }
 }
